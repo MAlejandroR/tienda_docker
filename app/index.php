cf7a034ea85748ae288006e7e35c8fe45d184012
MAlejandroR <manuelromeromiguel@gmail.com>
2022-02-09 14:41:21 +0100

    primer commit v1 docker

---
+++ app/index.php
@@ -0,0 +1,23 @@
+<?php
+
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<form action="index.php" method="post">
+    <fieldset>
+        <legend>Datos de acceso</legend>
+        <label for="usuario">Nombre de usuario</label> <input type="text" name="name" id=""><br />
+        <label for="password">Password</label> <input type="text" name="pass" id=""><br />
+        <input type="submit" value="Acceder" name="submit">
+    </fieldset>
+</form>
+</body>
+</html>
